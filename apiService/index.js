af0cd8aed8cd92c9341930876b8ff21996cd46f8
X-Keyscore <antonin.raymondaud@gmail.com>
2023-12-21 12:49:47 +0100

    edit cors policy 5

--- apiService/index.js
+++ apiService/index.js
@@ -14,11 +14,13 @@ const { URL_AUDIENCE, API_PORT } = process.env;
 export default function startApi() {
 	app.use(helmet());
 
+	/*
 	const origin = cors({
 		origin: URL_AUDIENCE,
 		optionsSuccessStatus: 200 // For legacy browser support
 	})
-	app.use(origin)
+	app.use(origin)*/
+	app.use(cors());
 
 	const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
