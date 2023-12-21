c56ee3a0d85e9eac791b461774d4ab7a698261e6
X-Keyscore <antonin.raymondaud@gmail.com>
2023-12-21 12:31:09 +0100

    edit cors policy 4

--- apiService/index.js
+++ apiService/index.js
@@ -14,22 +14,20 @@ const { URL_AUDIENCE, API_PORT } = process.env;
 export default function startApi() {
 	app.use(helmet());
 
-	/*
 	const origin = cors({
 		origin: URL_AUDIENCE,
 		optionsSuccessStatus: 200 // For legacy browser support
 	})
-	app.use(origin)*/
-	app.use(cors());
+	app.use(origin)
 
 	const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limite chaque IP à 100 requêtes par windowMs
 	});
 	app.use("/api/", limiter);
-	
+
 	app.use(Cache);
-	
+
 	const server = http.createServer(app);
 	
 	db.on('error', console.error.bind(console, 'MongoDB connection error:'));
